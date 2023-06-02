81478ecc9c42d23c94cedf2b9b760b33e585f150
Quoc Hung <lamquochung287@gmail.com>
2023-06-02 17:38:08 +0700

    banner component and css navbar components

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import Logo from '../Logo/Logo'
 import './Navbar.scss'
 export const Navbar = () => {
+  const isLogin = false
   return (
     <nav className="navbar_container">
       <Logo />
@@ -14,11 +15,22 @@ export const Navbar = () => {
       <div className="navbar_tab">
         Your Result
       </div>
-      <img
-        src="default_avatar.png"
-        alt="avatar"
-        className="avatar"
-      />
+      {isLogin ? (
+        <img
+          src="default_avatar.png"
+          alt="avatar"
+          className="avatar"
+        />
+      ) : (
+        <div className="navbar_btn_container">
+          <button className="navbar_btn blackText_whiteBackground">
+            Log in
+          </button>
+          <button className="navbar_btn whiteText_blackBackground">
+            Sign up
+          </button>
+        </div>
+      )}
     </nav>
   )
 }
--- client/src/page/HomePage/HomePage.jsx
+++ client/src/page/HomePage/HomePage.jsx
@@ -3,7 +3,21 @@ import './HomePage.scss'
 export const HomePage = () => {
   return (
     <div className="homepage_container">
-      HomePage
+      <div className="homepage_banner">
+        <img
+          src="banner.jpg"
+          alt="banner advertise"
+        />
+        <div className="banner_slogan">
+          <p className="banner_slogan_tittle">
+            Learning that gets you
+          </p>
+          <p className="banner_slogan_text">
+            Skills for your present (and your
+            future). Get started with us.
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
